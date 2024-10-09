252104f7a91aaf82dae6530e059c8742c82cb1c3
vinivid <frato.vini@gmail.com>
2024-10-08 22:02:14 -0300

    com checador de erros

--- main.c
+++ main.c
@@ -5,10 +5,7 @@
 int main(void) {
 	mapa o_mapa = ler_entrada();
 	clock_t a, b;
-	a = clock();
 	res o_caminho = caminho_mais_curto(o_mapa.tam, o_mapa.perm_inicial, o_mapa.lista_mapa);
-	b = clock();
-	printf("%d %lf\n", o_mapa.tam, (double)(b - a)/CLOCKS_PER_SEC);
-	//printar_resposta(o_mapa.tam, o_caminho);
+	printar_resposta(o_mapa.tam, o_caminho);
 }
 
