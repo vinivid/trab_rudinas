8c8a9d99f4fde428626080e0645ecd84daad3c33
vinivid <frato.vini@gmail.com>
2024-10-08 23:01:59 -0300

    main normal e comentado, só falta comentar o menor dist

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include "menor_dist.h"
-#include <time.h>
-#include <stdio.h>
 
 int main(void) {
 	mapa o_mapa = ler_entrada();
-	clock_t a, b;
 	res o_caminho = caminho_mais_curto(o_mapa.tam, o_mapa.perm_inicial, o_mapa.lista_mapa);
 	printar_resposta(o_mapa.tam, o_caminho);
 }
